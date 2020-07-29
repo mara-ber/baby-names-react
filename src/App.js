7927d324742f40a1f8a9dccf3b1e29ccde026b05
Marina <mara.berezin@gmail.com>
2020-07-29 21:52:35 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Heading from './Heading.js';
+import Names from './Names.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Heading />
+      <Names />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Heading.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import logo from './logo.svg';
+
+const Heading = () => {
+    return (
+        <header className="App-header">
+            {/* <img src="https://images.pexels.com/photos/1556706/pexels-photo-1556706.jpeg?cs=srgb&dl=baby-lying-on-pink-bed-1556706.jpg&fm=jpg" className="App-logo" alt="logo" /> */}
+            <p>
+                Baby names
+        </p>
+
+        </header>
+    )
+}
+
+export default Heading;
