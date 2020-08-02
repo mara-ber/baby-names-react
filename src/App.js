9484cd29e09f52547cee3675a2bc09ddec0952af
Marina <mara.berezin@gmail.com>
2020-08-02 13:54:59 +0100

    search works; level2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import Heading from './Heading.js';
 import NamesSorted from './NamesSorted.js';
-import SearchField from './SearchField.js';
+import Search from './SearchField.js';
 
 function App() {
   return (
     <div className="App">
       <Heading />
-      <SearchField />
-      <NamesSorted />
+      <Search />
+      {/* <NamesSorted /> */}
 
     </div>
   );
--- src/SearchField.js
+++ src/SearchField.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import babyNamesData from './babyNamesData.json';
 
 
-const SearchField = () => {
-    return <input type="search" placeholder="Search"></input>
+function Search() {
+    const [value, setValue] = useState("");
+    //console.log(value);
 
-};
+    function handleChange(event) {
+        setValue(event.target.value);
+    }
 
-export default SearchField;
+    return (
+        <div>
+            <input
+                onChange={handleChange}
+                value={value}
+                type="search"
+                placeholder="Search">
+            </input>
+            <div className="Names"> {
+                babyNamesData
+                    .filter(function (el) {
+                        return el.name.toLowerCase().includes(value.toLowerCase());
+                    })
+                    .sort((a, b) => a.name > b.name ? 1 : -1)
+                    .map(function (el, i) {
+                        if (el.sex === "m") {
+                            return <div className="NameM" key={i}> {el.name} </div>;
+                        }
+                        else {
+                            return <div className="NameF" key={i}> {el.name} </div>;
+                        }
+                    })
+            } </div>
+        </div>
+
+    );
+
+}
+
+export default Search;
